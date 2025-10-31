51cd8468b2941b12973db56dc476d82a9cf2fbd0
charantej Padepalli <cherry18tejs@gmail.com>
2025-10-31 17:04:25 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+component = $1
 dnf install ansible -y
-ansible-pull -U https://github.com/charantejpadepalli/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/charantejpadepalli/ansible-roboshop-roles-tf.git -e component=$component main.yaml
