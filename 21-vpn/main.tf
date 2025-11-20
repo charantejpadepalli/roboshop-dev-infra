c11f5eae66fcd4a0905b47297feaf4258492a664
charantej Padepalli <cherry18tejs@gmail.com>
2025-11-20 15:48:30 +0530

    terraform

--- 21-vpn/main.tf
+++ 21-vpn/main.tf
@@ -12,3 +12,11 @@ resource "aws_instance" "bastion" {
     }
   )
 }
+resource "aws_route53_record" "openvpn" {
+  zone_id = var.zone_id
+  name    = "openvpn.${var.domain_name}" # openvpn.daws86s.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.openvpn.public_ip]
+  allow_overwrite = true
+}
--- 21-vpn/variables.tf
+++ 21-vpn/variables.tf
@@ -4,3 +4,9 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+variable "zone_id" {
+  default = "Z07634881IMZZFH5TBV0T"
+}
+variable "domain_name" {
+  default = "devopspractice.shop"
+}
