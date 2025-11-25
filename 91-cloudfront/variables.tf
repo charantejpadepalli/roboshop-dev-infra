737c1e2bc7d9992162caf7baa47d3b4a2acb412e
charantej Padepalli <cherry18tejs@gmail.com>
2025-11-25 13:01:28 +0530

    terraform

---
+++ 91-cloudfront/variables.tf
@@ -0,0 +1,12 @@
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+variable "zone_id" {
+  default = "Z07634881IMZZFH5TBV0T"
+}
+variable "domain_name" {
+  default = "devopspractice.shop"
+}
