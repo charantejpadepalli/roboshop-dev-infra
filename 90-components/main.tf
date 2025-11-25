c82a8d67f5be043da329d88dc1099f9d4b678308
charantej Padepalli <cherry18tejs@gmail.com>
2025-11-25 13:34:24 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
   for_each = var.components
   source = "git::https://github.com/charantejpadepalli/terraform-roboshop-component.git?ref=main"
-  component = each_key
+  component = each.key
   rule_priority = each.value.rule_priority
 }
